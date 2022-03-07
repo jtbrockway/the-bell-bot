0890ecdce5d9d5765b0f492d839aa4fd0137e90e
Jack Brockway <jtbrockway22@gmail.com>
2022-03-07 12:40:54 -0800

    Removed tokens

--- config/config.js
+++ config/config.js
@@ -1,10 +1,7 @@
 const config = {
   botToken: process.env.BOT_TOKEN,
   clientId: process.env.CLIENT_ID,
-  coreId: '<@&408875003619180544>',
+  coreId: '<@&408875003619180544>'
 };
-
-config.botToken = 'OTQ5NzY3MzIzNjMwMzI1ODgx.YiPJvw.rncx2ulDnZ2mZ2oTX0BsQOIOhIg';
-config.clientId = '949767323630325881';
 
 export default config;
